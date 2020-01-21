ff79586a95538f483f06c417f10c636154228cfa
Johan Bos <jbos09@student.rocvantwente.nl>
2020-01-21 12:42:01 +0100

    new

--- yahtzeegame9ao9/yahtzeegame9ao9/Form1.Designer.cs
+++ yahtzeegame9ao9/yahtzeegame9ao9/Form1.Designer.cs
@@ -74,6 +74,8 @@
             this.pdDiceIns5 = new System.Windows.Forms.PictureBox();
             this.pdDiceIns6 = new System.Windows.Forms.PictureBox();
             this.btnReset = new System.Windows.Forms.Button();
+            this.tbRemaining = new System.Windows.Forms.TextBox();
+            this.lbCount = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbDice5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbDice4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbDice3)).BeginInit();
@@ -91,9 +93,11 @@
             // 
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label6.Location = new System.Drawing.Point(572, 184);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(57, 17);
+            this.label6.Size = new System.Drawing.Size(66, 17);
             this.label6.TabIndex = 59;
             this.label6.Text = "total 6\'s";
             // 
@@ -101,9 +105,11 @@
             // 
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label5.Location = new System.Drawing.Point(466, 184);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(57, 17);
+            this.label5.Size = new System.Drawing.Size(66, 17);
             this.label5.TabIndex = 58;
             this.label5.Text = "total 5\'s";
             // 
@@ -111,9 +117,11 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label4.Location = new System.Drawing.Point(361, 184);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.Size = new System.Drawing.Size(66, 17);
             this.label4.TabIndex = 57;
             this.label4.Text = "total 4\'s";
             // 
@@ -121,9 +129,11 @@
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label3.Location = new System.Drawing.Point(255, 184);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.Size = new System.Drawing.Size(66, 17);
             this.label3.TabIndex = 56;
             this.label3.Text = "total 3\'s";
             // 
@@ -131,9 +141,11 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label2.Location = new System.Drawing.Point(144, 184);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.Size = new System.Drawing.Size(66, 17);
             this.label2.TabIndex = 55;
             this.label2.Text = "total 2\'s";
             // 
@@ -141,9 +153,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label1.Location = new System.Drawing.Point(40, 184);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.Size = new System.Drawing.Size(66, 17);
             this.label1.TabIndex = 54;
             this.label1.Text = "total 1\'s";
             // 
@@ -347,9 +361,11 @@
             // 
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label7.Location = new System.Drawing.Point(24, 254);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(74, 17);
+            this.label7.Size = new System.Drawing.Size(85, 17);
             this.label7.TabIndex = 60;
             this.label7.Text = "3 of a kind";
             // 
@@ -357,9 +373,11 @@
             // 
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label8.Location = new System.Drawing.Point(134, 254);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(74, 17);
+            this.label8.Size = new System.Drawing.Size(85, 17);
             this.label8.TabIndex = 61;
             this.label8.Text = "4 of a kind";
             // 
@@ -367,9 +385,11 @@
             // 
             this.label9.AutoSize = true;
             this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label9.Location = new System.Drawing.Point(246, 254);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(75, 17);
+            this.label9.Size = new System.Drawing.Size(85, 17);
             this.label9.TabIndex = 62;
             this.label9.Text = "Full House";
             // 
@@ -377,9 +397,11 @@
             // 
             this.label10.AutoSize = true;
             this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label10.Location = new System.Drawing.Point(39, 321);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(58, 17);
+            this.label10.Size = new System.Drawing.Size(65, 17);
             this.label10.TabIndex = 63;
             this.label10.Text = "yahtzee";
             // 
@@ -387,9 +409,11 @@
             // 
             this.label11.AutoSize = true;
             this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label11.Location = new System.Drawing.Point(133, 321);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(80, 17);
+            this.label11.Size = new System.Drawing.Size(92, 17);
             this.label11.TabIndex = 64;
             this.label11.Text = "large street";
             // 
@@ -397,9 +421,11 @@
             // 
             this.label12.AutoSize = true;
             this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label12.Location = new System.Drawing.Point(238, 321);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(82, 17);
+            this.label12.Size = new System.Drawing.Size(94, 17);
             this.label12.TabIndex = 65;
             this.label12.Text = "Small street";
             // 
@@ -407,9 +433,11 @@
             // 
             this.label13.AutoSize = true;
             this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label13.Location = new System.Drawing.Point(24, 373);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(35, 17);
+            this.label13.Size = new System.Drawing.Size(40, 17);
             this.label13.TabIndex = 66;
             this.label13.Text = "total";
             // 
@@ -432,6 +460,7 @@
             this.pdDiceIns1.Size = new System.Drawing.Size(100, 100);
             this.pdDiceIns1.TabIndex = 68;
             this.pdDiceIns1.TabStop = false;
+            this.pdDiceIns1.Visible = false;
             // 
             // pdDiceIns2
             // 
@@ -442,6 +471,7 @@
             this.pdDiceIns2.Size = new System.Drawing.Size(100, 100);
             this.pdDiceIns2.TabIndex = 69;
             this.pdDiceIns2.TabStop = false;
+            this.pdDiceIns2.Visible = false;
             // 
             // pdDiceIns3
             // 
@@ -451,6 +481,7 @@
             this.pdDiceIns3.Size = new System.Drawing.Size(100, 100);
             this.pdDiceIns3.TabIndex = 70;
             this.pdDiceIns3.TabStop = false;
+            this.pdDiceIns3.Visible = false;
             // 
             // pdDiceIns4
             // 
@@ -460,6 +491,7 @@
             this.pdDiceIns4.Size = new System.Drawing.Size(100, 100);
             this.pdDiceIns4.TabIndex = 71;
             this.pdDiceIns4.TabStop = false;
+            this.pdDiceIns4.Visible = false;
             // 
             // pdDiceIns5
             // 
@@ -469,6 +501,7 @@
             this.pdDiceIns5.Size = new System.Drawing.Size(100, 100);
             this.pdDiceIns5.TabIndex = 72;
             this.pdDiceIns5.TabStop = false;
+            this.pdDiceIns5.Visible = false;
             // 
             // pdDiceIns6
             // 
@@ -478,22 +511,46 @@
             this.pdDiceIns6.Size = new System.Drawing.Size(100, 100);
             this.pdDiceIns6.TabIndex = 73;
             this.pdDiceIns6.TabStop = false;
+            this.pdDiceIns6.Visible = false;
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(12, 68);
+            this.btnReset.Location = new System.Drawing.Point(12, 113);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(100, 41);
             this.btnReset.TabIndex = 80;
             this.btnReset.Text = "Reset";
             this.btnReset.UseVisualStyleBackColor = true;
             // 
+            // tbRemaining
+            // 
+            this.tbRemaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbRemaining.Location = new System.Drawing.Point(12, 69);
+            this.tbRemaining.Name = "tbRemaining";
+            this.tbRemaining.Size = new System.Drawing.Size(100, 20);
+            this.tbRemaining.TabIndex = 81;
+            this.tbRemaining.Text = " ";
+            this.tbRemaining.TextChanged += new System.EventHandler(this.tbRemaining_TextChanged);
+            // 
+            // lbCount
+            // 
+            this.lbCount.AutoSize = true;
+            this.lbCount.Location = new System.Drawing.Point(39, 94);
+            this.lbCount.Name = "lbCount";
+            this.lbCount.Size = new System.Drawing.Size(54, 17);
+            this.lbCount.TabIndex = 82;
+            this.lbCount.Text = "label14";
+            this.lbCount.Click += new System.EventHandler(this.lbCount_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::yahtzeegame9ao9.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbCount);
+            this.Controls.Add(this.tbRemaining);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.pdDiceIns6);
             this.Controls.Add(this.pdDiceIns5);
@@ -605,6 +662,8 @@
         private System.Windows.Forms.PictureBox pdDiceIns5;
         private System.Windows.Forms.PictureBox pdDiceIns6;
         private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox tbRemaining;
+        private System.Windows.Forms.Label lbCount;
     }
 }
 
--- yahtzeegame9ao9/yahtzeegame9ao9/Form1.cs
+++ yahtzeegame9ao9/yahtzeegame9ao9/Form1.cs
@@ -13,7 +13,8 @@ namespace yahtzeegame9ao9
     public partial class Form1 : Form
     {
         System.Media.SoundPlayer player = new System.Media.SoundPlayer();
-
+        int counter = 4;
+        int remaining = 3;
         bool hold1 = false;
         bool hold2 = false;
         bool hold3 = false;
@@ -25,100 +26,111 @@ namespace yahtzeegame9ao9
 
             player.SoundLocation = "bensound-theelevatorbossanova.wav";
         }
-         private void btnRegels_Click(object sender, EventArgs e)
-                {
-                    System.Diagnostics.Process.Start("https://www.spelregels.eu/PDF/spelregels-yahtzee.pdf");
-                }
+        private void btnRegels_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.spelregels.eu/PDF/spelregels-yahtzee.pdf");
+        }
         private void btnRoll_Click(object sender, EventArgs e)
         {
             Random r = new Random();
             int iRnd = new int();
-
-            iRnd = r.Next(0, 6);
-            if (hold1 != true)
+            if (counter > 1)
             {
-
-                if (iRnd == 0)
-                { pbDice1.Image = pdDiceIns1.Image; }
-                else if (iRnd == 1)
-                { pbDice1.Image = pdDiceIns2.Image; }
-                else if (iRnd == 2)
-                { pbDice1.Image = pdDiceIns3.Image; }
-                else if (iRnd == 3)
-                { pbDice1.Image = pdDiceIns4.Image; }
-                else if (iRnd == 4)
-                { pbDice1.Image = pdDiceIns5.Image; }
-                else
-                { pbDice1.Image = pdDiceIns6.Image; }
+                remaining--;
+                tbRemaining.Text = remaining.ToString();
             }
 
-            if (hold2 != true)
-            {
-                iRnd = r.Next(0, 6);
-                if (iRnd == 0)
-                { pbDice2.Image = pdDiceIns1.Image; }
-                else if (iRnd == 1)
-                { pbDice2.Image = pdDiceIns2.Image; }
-                else if (iRnd == 2)
-                { pbDice2.Image = pdDiceIns3.Image; }
-                else if (iRnd == 3)
-                { pbDice2.Image = pdDiceIns4.Image; }
-                else if (iRnd == 4)
-                { pbDice2.Image = pdDiceIns5.Image; }
-                else
-                { pbDice2.Image = pdDiceIns6.Image; }
-            }
+            counter--;
 
-            if (hold3 != true)
-            {
-                iRnd = r.Next(0, 6);
-                if (iRnd == 0)
-                { pbDice3.Image = pdDiceIns1.Image; }
-                else if (iRnd == 1)
-                { pbDice3.Image = pdDiceIns2.Image; }
-                else if (iRnd == 2)
-                { pbDice3.Image = pdDiceIns3.Image; }
-                else if (iRnd == 3)
-                { pbDice3.Image = pdDiceIns4.Image; }
-                else if (iRnd == 4)
-                { pbDice3.Image = pdDiceIns5.Image; }
-                else
-                { pbDice3.Image = pdDiceIns6.Image; }
-            }
+            if (counter > 0)
+                {
+                    iRnd = r.Next(0, 6);
+                    if (hold1 != true)
+                    {
 
-            if (hold4 != true)
-            {
-                iRnd = r.Next(0, 6);
-                if (iRnd == 0)
-                { pbDice4.Image = pdDiceIns1.Image; }
-                else if (iRnd == 1)
-                { pbDice4.Image = pdDiceIns2.Image; }
-                else if (iRnd == 2)
-                { pbDice4.Image = pdDiceIns3.Image; }
-                else if (iRnd == 3)
-                { pbDice4.Image = pdDiceIns4.Image; }
-                else if (iRnd == 4)
-                { pbDice4.Image = pdDiceIns5.Image; }
-                else
-                { pbDice4.Image = pdDiceIns6.Image; }
-            }
+                        if (iRnd == 0)
+                        { pbDice1.Image = pdDiceIns1.Image; }
+                        else if (iRnd == 1)
+                        { pbDice1.Image = pdDiceIns2.Image; }
+                        else if (iRnd == 2)
+                        { pbDice1.Image = pdDiceIns3.Image; }
+                        else if (iRnd == 3)
+                        { pbDice1.Image = pdDiceIns4.Image; }
+                        else if (iRnd == 4)
+                        { pbDice1.Image = pdDiceIns5.Image; }
+                        else
+                        { pbDice1.Image = pdDiceIns6.Image; }
+                    }
 
-            if (hold5 != true)
-            {
-                iRnd = r.Next(0, 6);
-                if (iRnd == 0)
-                { pbDice5.Image = pdDiceIns1.Image; }
-                else if (iRnd == 1)
-                { pbDice5.Image = pdDiceIns2.Image; }
-                else if (iRnd == 2)
-                { pbDice5.Image = pdDiceIns3.Image; }
-                else if (iRnd == 3)
-                { pbDice5.Image = pdDiceIns4.Image; }
-                else if (iRnd == 4)
-                { pbDice5.Image = pdDiceIns5.Image; }
-                else
-                { pbDice5.Image = pdDiceIns6.Image; }
-            }
+                    if (hold2 != true)
+                    {
+                        iRnd = r.Next(0, 6);
+                        if (iRnd == 0)
+                        { pbDice2.Image = pdDiceIns1.Image; }
+                        else if (iRnd == 1)
+                        { pbDice2.Image = pdDiceIns2.Image; }
+                        else if (iRnd == 2)
+                        { pbDice2.Image = pdDiceIns3.Image; }
+                        else if (iRnd == 3)
+                        { pbDice2.Image = pdDiceIns4.Image; }
+                        else if (iRnd == 4)
+                        { pbDice2.Image = pdDiceIns5.Image; }
+                        else
+                        { pbDice2.Image = pdDiceIns6.Image; }
+                    }
+
+                    if (hold3 != true)
+                    {
+                        iRnd = r.Next(0, 6);
+                        if (iRnd == 0)
+                        { pbDice3.Image = pdDiceIns1.Image; }
+                        else if (iRnd == 1)
+                        { pbDice3.Image = pdDiceIns2.Image; }
+                        else if (iRnd == 2)
+                        { pbDice3.Image = pdDiceIns3.Image; }
+                        else if (iRnd == 3)
+                        { pbDice3.Image = pdDiceIns4.Image; }
+                        else if (iRnd == 4)
+                        { pbDice3.Image = pdDiceIns5.Image; }
+                        else
+                        { pbDice3.Image = pdDiceIns6.Image; }
+                    }
+
+                    if (hold4 != true)
+                    {
+                        iRnd = r.Next(0, 6);
+                        if (iRnd == 0)
+                        { pbDice4.Image = pdDiceIns1.Image; }
+                        else if (iRnd == 1)
+                        { pbDice4.Image = pdDiceIns2.Image; }
+                        else if (iRnd == 2)
+                        { pbDice4.Image = pdDiceIns3.Image; }
+                        else if (iRnd == 3)
+                        { pbDice4.Image = pdDiceIns4.Image; }
+                        else if (iRnd == 4)
+                        { pbDice4.Image = pdDiceIns5.Image; }
+                        else
+                        { pbDice4.Image = pdDiceIns6.Image; }
+                    }
+
+                    if (hold5 != true)
+                    {
+                        iRnd = r.Next(0, 6);
+                        if (iRnd == 0)
+                        { pbDice5.Image = pdDiceIns1.Image; }
+                        else if (iRnd == 1)
+                        { pbDice5.Image = pdDiceIns2.Image; }
+                        else if (iRnd == 2)
+                        { pbDice5.Image = pdDiceIns3.Image; }
+                        else if (iRnd == 3)
+                        { pbDice5.Image = pdDiceIns4.Image; }
+                        else if (iRnd == 4)
+                        { pbDice5.Image = pdDiceIns5.Image; }
+                        else
+                        { pbDice5.Image = pdDiceIns6.Image; }
+                    }
+                }
+            
 
 
         }
@@ -126,47 +138,28 @@ namespace yahtzeegame9ao9
         private void btnHold_Click(object sender, EventArgs e)
         {
             hold1 = true;
+            btnHold.Text = "Holded";
         }
 
         private void btnHold2_Click(object sender, EventArgs e)
         {
             hold2 = true;
+            btnHold2.Text = "Holded";
         }
         private void btnHold3_Click(object sender, EventArgs e)
         {
-            hold3 = true;
+            hold3 = true; 
+            btnHold3.Text = "Holded";
         }
         private void btnHold4_Click(object sender, EventArgs e)
         {
             hold4 = true;
+            btnHold4.Text = "Holded";
         }
         private void btnHold5_Click(object sender, EventArgs e)
         {
             hold5 = true;
-        }
-
-        private void btnHold4_Click_1(object sender, EventArgs e)
-        {
-            hold4 = true;
-        }
-
-        private void btnHold3_Click_1(object sender, EventArgs e)
-        {
-            hold3 = true;
-        }
-
-        private void btnHold5_Click_1(object sender, EventArgs e)
-        {
-            hold5 = true;
-        }
-
-        private void btReset_Click(object sender, EventArgs e)
-        {
-            hold1 = false;
-            hold2 = false;
-            hold3 = false;
-            hold4 = false;
-            hold5 = false;
+            btnHold5.Text = "Holded";
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -177,6 +170,31 @@ namespace yahtzeegame9ao9
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnReset_Click_1(object sender, EventArgs e)
+        {
+            hold1 = false;
+            hold2 = false;
+            hold3 = false;
+            hold4 = false;
+            hold5 = false;
+            btnHold.Text = "Hold";
+            btnHold2.Text = "Hold";
+            btnHold3.Text = "Hold";
+            btnHold4.Text = "Hold";
+            btnHold5.Text = "Hold";
+            counter = 4;
+            remaining = 3;
+        }
+
+        private void tbRemaining_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void lbCount_Click(object sender, EventArgs e)
+        {
+            //lbCount.Text = ToString (counter)
         }
     }
 }
